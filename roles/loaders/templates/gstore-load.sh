ae4f33f5285b735c27b7107abf071dcaf94d9c37
Lennart Austenfeld <lennarta@mail.upb.de>
2020-02-18 15:43:29 +0100

    Add Services

--- roles/loaders/templates/gstore-load.sh
+++ roles/loaders/templates/gstore-load.sh
@@ -2,8 +2,11 @@
 
 mkdir -p {{ database_base_dir }}/gstore/{{ item[1].name }}
 mkdir -p {{ database_base_dir }}/gstore/{{ item[1].name }}/.tmp
+mkdir -p {{ database_base_dir }}/gstore/{{ item[1].name }}/logs/endpoint
 cd {{ database_base_dir }}/gstore/{{ item[1].name }}
 
-time /opt/gstore/bin/gbuild {{ item[1].name }} {{ item[1].path }}
+cp -r /opt/gstore/data {{ database_base_dir }}/gstore/{{ item[1].name }}
 
-rm -r {{ database_base_dir }}/gstore/{{ item[1].name }}/.tmp
+/opt/gstore/bin/ginit
+
+time /opt/gstore/bin/gbuild {{ item[1].name }} {{ item[1].path }}
