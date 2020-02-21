cabf145f311bbed1578f0d558a01f9931c8f10d5
Lennart Austenfeld <lennarta@mail.upb.de>
2020-02-21 13:26:19 +0100

    Change GraphDB to free version

--- roles/loaders/templates/graphdb-load.sh
+++ roles/loaders/templates/graphdb-load.sh
@@ -2,4 +2,4 @@
 
 sudo -u graphdb mkdir -p {{ database_base_dir }}/graphdb/{{ item[1].name }}/data/
 
-sudo -u graphdb bash -c "time /opt/graphdb/graphdb-se-{{ graphdb_version }}/bin/loadrdf -Dgraphdb.home.data={{ database_base_dir }}/graphdb/{{ item[1].name }}/data/ -f -m parallel -c /opt/graphdb/graphdb-{{ item[1].name }}.ttl {{ item[1].path }}"
+sudo -u graphdb bash -c "time java -cp \"/opt/graphdb/graphdb-free-{{ graphdb_version }}/lib/*\" -Dgraphdb.dist=/opt/graphdb/graphdb-free-{{ graphdb_version }} -Dgraphdb.home.data={{ database_base_dir }}/graphdb/{{ item[1].name }}/data/ -Djdk.xml.entityExpansionLimit=0 com.ontotext.graphdb.loadrdf.LoadRDF -f -m parallel -c /opt/graphdb/graphdb-{{ item[1].name }}.ttl {{ item[1].path }}"
