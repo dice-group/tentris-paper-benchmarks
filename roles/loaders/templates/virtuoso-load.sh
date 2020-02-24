dd5e2ca108910322c2fb5a0daa73bde2f0efcf96
Lennart Austenfeld <lennarta@mail.upb.de>
2020-02-24 12:58:50 +0100

    Update Virtuoso

--- roles/loaders/templates/virtuoso-load.sh
+++ roles/loaders/templates/virtuoso-load.sh
@@ -8,7 +8,7 @@ sudo systemctl start virtuoso@{{ item[1].name }}.service
 
 sleep 30 # Wait for server to come online
 
-time /opt/virtuoso/7.2.5/virtuoso-opensource/bin/isql << EOF
+time /opt/virtuoso/{{ virtuoso_version }}/virtuoso-opensource/bin/isql << EOF
 ld_dir ('{{ item[1].path | regex_replace('^(.*)/.*\.nt$', '\\1') }}', '*.nt', 'http://dbpedia.org');
 rdf_loader_run();
 checkpoint;
