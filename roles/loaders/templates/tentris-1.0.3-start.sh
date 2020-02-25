241b1c99357e04ebe99f804d5d2e569b6e03b169
Lennart Austenfeld <lennarta@mail.upb.de>
2020-02-25 13:33:34 +0100

    Fix spaces in Tentris start script

--- roles/loaders/templates/tentris-1.0.3-start.sh
+++ roles/loaders/templates/tentris-1.0.3-start.sh
@@ -4,8 +4,8 @@ sudo systemctl start tentris-1.0.3@{{ item[1].path | replace('/', '-') }}.servic
 
 while :
 do
-    curl 127.0.0.1:9080
-    if [$? -eq 0]
+    curl -q 127.0.0.1:9080
+    if [ $? -eq 0 ]
     then
         break
     fi
--- roles/loaders/templates/tentris-1.0.3-tsl_sparse_set-start.sh
+++ roles/loaders/templates/tentris-1.0.3-tsl_sparse_set-start.sh
@@ -4,8 +4,8 @@ sudo systemctl start tentris-1.0.3-tsl_sparse_set@{{ item[1].path | replace('/',
 
 while :
 do
-    curl 127.0.0.1:9080
-    if [$? -eq 0]
+    curl -q 127.0.0.1:9080
+    if [ $? -eq 0 ]
     then
         break
     fi
