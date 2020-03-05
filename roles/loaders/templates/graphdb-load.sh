1332913a258f02d25956e3aa8f0fcb7d0ad48a6a
Alexander Bigerl <bigerl@mail.uni-paderborn.de>
2020-03-05 20:32:07 +0100

    fixed graphdb-load.sh

--- roles/loaders/templates/graphdb-load.sh
+++ roles/loaders/templates/graphdb-load.sh
@@ -2,4 +2,4 @@
 
 mkdir -p databases/graphdb/{{ item[1].name }}/data/
 
-cgmemtime java -cp "/opt/graphdb/graphdb-free-{{ graphdb_version }}/lib/*" -Dgraphdb.dist=/opt/graphdb/graphdb-free-{{ graphdb_version }} -Dgraphdb.home.data=$(pwd)/databases/graphdb/{{ item[1].name }}/data/ -Djdk.xml.entityExpansionLimit=0 com.ontotext.graphdb.loadrdf.LoadRDF -f -m parallel -p -c /opt/graphdb/graphdb-{{ item[1].name }}.ttl {{ item[1].path }} 2>&1 | tee graphdb-load-{{ item[1].name }}.log
+cgmemtime /opt/graphdb/graphdb-free-{{ graphdb_version }}/bin/loadrdf -f -m parallel -p -c /opt/graphdb/graphdb-{{ item[1].name }}.ttl {{ item[1].path }} 2>&1 | tee graphdb-load-{{ item[1].name }}.log
