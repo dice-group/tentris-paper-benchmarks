828a806e2222b6e5b6642d5b1b39c20209570329
Lennart Austenfeld <lennarta@mail.upb.de>
2020-03-09 15:24:22 +0100

    Revert "fixed graphdb-load.sh"
    This reverts commit 1332913a258f02d25956e3aa8f0fcb7d0ad48a6a.

--- roles/loaders/templates/graphdb-load.sh
+++ roles/loaders/templates/graphdb-load.sh
@@ -2,4 +2,4 @@
 
 mkdir -p databases/graphdb/{{ item[1].name }}/data/
 
-cgmemtime sudo -u $USER /opt/graphdb/graphdb-free-{{ graphdb_version }}/bin/loadrdf -X mx512G -f -m parallel -p -c /opt/graphdb/graphdb-{{ item[1].name }}.ttl {{ item[1].path }} 2>&1 | tee {{ target_dir }}/logs/graphdb-load-{{ item[1].name }}.log
+cgmemtime java -cp "/opt/graphdb/graphdb-free-{{ graphdb_version }}/lib/*" -Dgraphdb.dist=/opt/graphdb/graphdb-free-{{ graphdb_version }} -Dgraphdb.home.data=$(pwd)/databases/graphdb/{{ item[1].name }}/data/ -Djdk.xml.entityExpansionLimit=0 com.ontotext.graphdb.loadrdf.LoadRDF -f -m parallel -p -c /opt/graphdb/graphdb-{{ item[1].name }}.ttl {{ item[1].path }} 2>&1 | tee graphdb-load-{{ item[1].name }}.log
