dc3f6644aabc44a8a54cc6d462ffb898b509fcab
Alexander Bigerl <bigerl@mail.uni-paderborn.de>
2020-04-07 15:55:29 +0200

    workaround for: CLI IGUANA cannot handle lists of single quote escaped strings.

--- roles/iguana/templates/iguana-run.sh
+++ roles/iguana/templates/iguana-run.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-java -cp {{ target_dir }}/iguana-{{ iguana_version }}/corecontroller.jar org.aksw.iguana.cc.controller.MainController $1
+java -Xmx{{ iguana_mem }} -cp {{ target_dir }}/iguana-{{ iguana_version }}/corecontroller.jar org.aksw.iguana.cc.controller.MainController $1
--- roles/loaders/templates/graphdb-preload.sh
+++ roles/loaders/templates/graphdb-preload.sh
@@ -2,4 +2,4 @@
 
 mkdir -p {{ target_dir }}/databases/graphdb/{{ item.name }}/data/
 
-cgmemtime java -Xmx512G -cp "{{ target_dir }}/triplestores/graphdb/graphdb-free-{{ graphdb_version }}/lib/*" -Dgraphdb.dist={{ target_dir }}/triplestores/graphdb/graphdb-free-{{ graphdb_version }} -Dgraphdb.home.data={{ target_dir }}/databases/graphdb/{{ item.name }}/data/ -Djdk.xml.entityExpansionLimit=0 com.ontotext.graphdb.loadrdf.PreloadData -f -p -c {{ target_dir }}/triplestores/graphdb/graphdb-{{ item.name }}.ttl {{ item.path }} 2>&1 | tee {{ target_dir }}/logs/load/graphdb-preload-{{ item.name }}.log
+cgmemtime java -Xmx{{ load_max_ram }}K -cp "{{ target_dir }}/triplestores/graphdb/graphdb-free-{{ graphdb_version }}/lib/*" -Dgraphdb.dist={{ target_dir }}/triplestores/graphdb/graphdb-free-{{ graphdb_version }} -Dgraphdb.home.data={{ target_dir }}/databases/graphdb/{{ item.name }}/data/ -Djdk.xml.entityExpansionLimit=0 com.ontotext.graphdb.loadrdf.PreloadData -f -p -c {{ target_dir }}/triplestores/graphdb/graphdb-{{ item.name }}.ttl {{ item.path }} 2>&1 | tee {{ target_dir }}/logs/load/graphdb-preload-{{ item.name }}.log
