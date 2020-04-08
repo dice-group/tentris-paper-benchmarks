9ab3a159cb80f4f36d67000c28f8d2e4de43056e
Alexander Bigerl <alexander@bigerl.eu>
2020-04-08 11:01:26 +0200

    Tweaks (#15)
    * workaround for: CLI IGUANA cannot handle lists of single quote escaped strings.
    * Move variables to roles and fix type
    * Workaround for inconsistent IGUANA version name
    * Improve Homebrew Installation
    * Rename file on template copy as well
    Co-authored-by: Alexander Bigerl <bigerl@mail.uni-paderborn.de>
    Co-authored-by: Lennart Austenfeld <lennarta@mail.upb.de>

--- roles/loaders/templates/blazegraph-load.sh
+++ roles/loaders/templates/blazegraph-load.sh
@@ -4,6 +4,6 @@ mkdir -p {{ target_dir }}/databases/blazegraph/{{ item[1].name }}
 dir=$(pwd)
 cd {{ target_dir }}/databases/blazegraph/{{ item[1].name }}
 
-cgmemtime java -Xmx512G -cp {{ target_dir }}/triplestores/blazegraph/blazegraph.jar -Djava.io.tmpdir=./ com.bigdata.rdf.store.DataLoader {{ target_dir }}/triplestores/blazegraph/application.properties {{ item[1].path }} 2>&1 | tee {{ target_dir }}/logs/load/blazegraph-{{ item[1].name }}.log
+cgmemtime java -Xmx{{ load_max_ram }}K -XX:MaxDirectMemorySize={{ load_max_ram }}K -cp {{ target_dir }}/triplestores/blazegraph/blazegraph.jar -Djava.io.tmpdir=./ com.bigdata.rdf.store.DataLoader {{ target_dir }}/triplestores/blazegraph/application.properties {{ item[1].path }} 2>&1 | tee {{ target_dir }}/logs/load/blazegraph-{{ item[1].name }}.log
 
 cd $pwd
