87781a32aaedbcb45b3075561b718ad7a8829f57
TortugaAttack <felixconrads@googlemail.com>
2020-11-24 13:26:40 +0100

    Iguana to version 3

--- roles/iguana/templates/iguana-run.sh
+++ roles/iguana/templates/iguana-run.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-java -Xmx{{ iguana_mem }} -cp {{ target_dir }}/iguana-{{ iguana_version }}/corecontroller.jar org.aksw.iguana.cc.controller.MainController $1
+java -Xmx{{ iguana_mem }} {{ target_dir }}/iguana-{{ iguana_version }}/corecontroller.jar  $1
