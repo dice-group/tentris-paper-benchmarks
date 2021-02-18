60efd21df8c80c7df8cc508aca2c79c20ebd0129
Alexander Bigerl <bigerl@mail.upb.de>
2021-02-18 13:33:12 +0100

    added logging

--- parse_sparql_results/templates/parse-sparql-results-tentris.sh
+++ parse_sparql_results/templates/parse-sparql-results-tentris.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-bash "{{ target_dir }}/iguana_suites/http/tentris-{{ item[0] }}/{{ item[1].name }}/{{ item[2].number }}-start.sh"
+bash "{{ target_dir }}/iguana_suites/http/tentris-{{ item[0] }}/{{ item[1].name }}/1-start.sh"
 
 cd "{{ target_dir }}" || exit
 
-sparql_result_analysis --url "{{ endpoint }}" --queries "{{ queries_path }}" --datasetname "{{ datasetname }}" --storename "{{ triplestore_name }}" --dont-save
+sparql_result_analysis --url "{{ endpoint }}" --queries "{{ queries_path }}" --datasetname "{{ datasetname }}" --storename "tentris-{{ item[0] }}" --dont-save 2>&1 | tee {{ target_dir }}/logs/parse_sparql_results/tentris-{{ item[0] }}-{{ datasetname }}.log
 
 bash "{{ target_dir }}/iguana_suites/http/tentris-{{ item[0] }}/stop.sh"
--- parse_sparql_results/templates/parse-sparql-results.sh
+++ parse_sparql_results/templates/parse-sparql-results.sh
@@ -4,6 +4,6 @@ bash "{{ target_dir }}/iguana_suites/http/{{ triplestore_name }}/{{ datasetname 
 
 cd "{{ target_dir }}" || exit
 
-sparql_result_analysis --url "{{ endpoint }}" --queries "{{ queries_path }}" --datasetname "{{ datasetname }}" --storename "{{ triplestore_name }}" --dont-save
+sparql_result_analysis --url "{{ endpoint }}" --queries "{{ queries_path }}" --datasetname "{{ datasetname }}" --storename "{{ triplestore_name }}" --dont-save 2>&1 | tee {{ target_dir }}/logs/parse_sparql_results/{{ triplestore_name }}-{{ datasetname }}.log
 
 bash "{{ target_dir }}/iguana_suites/http/{{ triplestore_name }}/stop.sh"
