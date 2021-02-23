ecf3708c28d0085a1a5ae93c09b660913bcc003b
Lennart Austenfeld <lennarta@mail.upb.de>
2021-02-23 10:20:44 +0100

    Replace placeholder in connection_url

--- roles/parse_sparql_results/templates/parse-sparql-results.sh
+++ roles/parse_sparql_results/templates/parse-sparql-results.sh
@@ -4,6 +4,6 @@ bash "{{ target_dir }}/iguana_suites/http/{{ item[0].name }}/{{ item[1].name }}/
 
 cd "{{ target_dir }}" || exit
 
-sparql_result_analysis --url "{{ item[0].connection_url }}" --queries "{{ item[1].queries_path }}" --datasetname "{{ item[1].name }}" --storename "{{ item[0].name }}" --dont-save 2>&1 | tee {{ target_dir }}/logs/parse_sparql_results/{{ item[0].name }}-{{ item[1].name }}.log
+sparql_result_analysis --url "{{ item[0].connection_url | replace('%d', item[1].name) }}" --queries "{{ item[1].queries_path }}" --datasetname "{{ item[1].name }}" --storename "{{ item[0].name }}" --dont-save 2>&1 | tee {{ target_dir }}/logs/parse_sparql_results/{{ item[0].name }}-{{ item[1].name }}.log
 
 bash "{{ target_dir }}/iguana_suites/http/{{ item[0].name }}/stop.sh"
