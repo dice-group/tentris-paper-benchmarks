f48e1be95229a759ea5db2dcc81a86da0341ea4b
Nick Molcanov <32801560+nck-mlcnv@users.noreply.github.com>
2023-07-15 13:01:01 +0200

    update tentris script template

--- roles/iguana_suites/templates/tentris-start.sh
+++ roles/iguana_suites/templates/tentris-start.sh
@@ -9,7 +9,14 @@ fi
 
 echo $(date --iso-8601) - Starting Tentris
 
-{{ target_dir }}/triplestores/tentris/{{ item[0] }}/tentris_server -l 1000 -f {{ item[1].path }} --logfile false --logstdout </dev/null 2>&1 >{{ target_dir }}/logs/run/tentris-{{ item[0] }}-{{ item[1].name }}-{{ item[2].number }}.log & disown
+{{ target_dir }}/triplestores/tentris/{{ item[0] }}/tentris_server \
+    --loglevel debug \
+    --storage {{ target_dir }}/databases/tentris/{{ item[0] }}/{{ item[1].name }} \
+    --logfile false \
+    --timeout 3600 \
+    --logstdout \
+    </dev/null 2>&1 >{{ target_dir }}/logs/run/tentris-{{ item[0] }}-{{ item[1].name }}.log & disown
+
 pid=$!
 
 echo $pid > {{ target_dir }}/tentris.pid
--- roles/loaders/templates/tentris-load.sh
+++ roles/loaders/templates/tentris-load.sh
@@ -1,4 +1,11 @@
 #! /bin/bash
 
-cgmemtime  {{ target_dir }}/triplestores/tentris/{{ item[0] }}/tentris_terminal --logfile false --logstdout -f {{ item[1].path }} << EOF 2>&1 | tee {{ target_dir }}/logs/load/tentris-{{ item[0] }}-{{ item[1].name }}.log
+mkdir -p {{ target_dir }}/databases/tentris/{{ item[0] }}
+
+{{ target_dir }}/triplestores/tentris/{{ item[0] }}/tentris_loader \
+    --logfile false \
+    --logstdout \
+    --file {{ item[1].path }} \
+    --storage {{ target_dir }}/databases/tentris/{{ item[0] }}/{{ item[1].name }} \
+    << EOF 2>&1 | tee {{ target_dir }}/logs/load/tentris-{{ item[0] }}-{{ item[1].name }}.log
 EOF
